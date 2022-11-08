47c1bdd5f5decfb5d212b1641969d9952ab91281
sergiomarotco <sergiomarotco@gmail.com>
2022-11-09 00:53:55 +0300

    add multi file parsing

--- TelegramChatParser/Program.cs
+++ TelegramChatParser/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using CommandLine;
 using E2.Utils;
 
@@ -11,24 +13,42 @@ namespace TelegramChatParser
         {
             try
             {
-                ParserResult<TgParser> parserResult =
-                    Parser
-                        .Default
-                        .ParseArguments<TgParser>(args)
-                        .WithParsed<TgParser>(o =>
-                        {
-                            if (!File.Exists(o.InputHtmlPath))
-                            {
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Error: inputHtml dose not exists! aborting...");
-                                Console.ForegroundColor = ConsoleColor.White;
-                                return;
-                            }
-                            
-                            TgParser tgParser = new TgParser(o.InputHtmlPath, o.CsvFilePath, o.Verbose, o.Append);
 
-                            tgParser.CreateCsv();
-                        });
+                string[] input_files = Directory.GetFiles(Environment.CurrentDirectory, "messages*.html", SearchOption.AllDirectories);
+                int files = 0;
+                if (input_files.Length != 0) //try parse all file from Environment.CurrentDirectory
+                {
+                    files = input_files.Length;
+                }
+                if (files == 0) //parse from arg
+                {
+                    ParserResult<TgParser> parserResult =
+                        Parser
+                            .Default
+                            .ParseArguments<TgParser>(args)
+                            .WithParsed<TgParser>(o =>
+                            {
+                                if (!File.Exists(o.InputHtmlPath))
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Error: inputHtml dose not exists! aborting...");
+                                    Console.ForegroundColor = ConsoleColor.White;
+                                    return;
+                                }
+
+                                TgParser tgParser = new TgParser(o.InputHtmlPath, o.CsvFilePath, o.Verbose, o.Append);
+
+                                tgParser.CreateCsv();
+                            });
+                }
+                else //parse all files in Environment.CurrentDirectory
+                {
+                    for (int i = 0; i < files; i++)
+                    {
+                        TgParser tgParser = new TgParser(@input_files[i], @input_files[i].Replace(".html", ".csv"), false, true); //Verbose=false, Append=true
+                        tgParser.CreateCsv();
+                    }
+                }
             }
             catch (Exception e)
             {
